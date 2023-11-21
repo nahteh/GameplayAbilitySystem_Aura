2cff17915c86f3aed751dd53009a0110a3804291
nahteh <nahteh@hotmail.com>
2023-11-21 11:23:17 -0800

    include errors

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,17 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+    bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+    super::BeginPlay();
+    check(AuraContext);
+
+    UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer())
+}
